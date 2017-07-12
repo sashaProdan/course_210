fd37c19d7b15a9084a7de9ac5865f6f567ebfd45
sashaprodan <prodan.sasha@gmail.com>
2017-07-12 08:08:26 -0500

    Processing Releases

---
+++ lesson_07/practice_problems/processing_releases.js
@@ -0,0 +1,53 @@
+var newReleases = [
+  {
+    'id': 70111470,
+    'title': 'Die Hard',
+    'boxart': 'http://cdn-0.nflximg.com/images/2891/DieHard.jpg',
+    'uri': 'http://api.netflix.com/catalog/titles/movies/70111470',
+    'rating': [4.0],
+    'bookmark': [],
+  },
+  {
+    'id': 654356453,
+    'boxart': 'http://cdn-0.nflximg.com/images/2891/BadBoys.jpg',
+    'uri': 'http://api.netflix.com/catalog/titles/movies/70111470',
+    'rating': [5.0],
+    'bookmark': [{ id:432534, time:65876586 }],
+  },
+  {
+    'title': 'The Chamber',
+    'boxart': 'http://cdn-0.nflximg.com/images/2891/TheChamber.jpg',
+    'uri': 'http://api.netflix.com/catalog/titles/movies/70111470',
+    'rating': [4.0],
+    'bookmark': [],
+  },
+  {
+    'id': 675465,
+    'title': 'Fracture',
+    'boxart': 'http://cdn-0.nflximg.com/images/2891/Fracture.jpg',
+    'uri': 'http://api.netflix.com/catalog/titles/movies/70111470',
+    'rating': [5.0],
+    'bookmark': [{ id:432534, time:65876586 }],
+  },
+];
+
+// Array of Objects that contain only the id and title key/value pairs
+// Keep only releases that have both id and title data present.
+// Keep only the id and title data for each release.
+
+// iterate through each obect
+// select objects that have 'id' and 'title' keys present
+// map each release to an object with only id and title
+
+function processReleaseData(data) {
+  return data.filter(function(release) {
+    return release.id && release.title;
+  }).map(function(release) {
+    return {id: release.id, title: release.title};
+  })
+};
+
+console.log(processReleaseData(newReleases));
+
+// should return:
+// [{ id: 70111470, title: 'Die Hard'}, { id: 675465, title: 'Fracture' }];
